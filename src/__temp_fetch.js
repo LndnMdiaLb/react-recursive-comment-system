4f5c10356669cb9cbd44ecf9bdd20d14fc1dc455
LndnMdiaLb <projects@thelondonmedialab.com>
2018-05-23 12:50:55 +0100

    rmove this file at some point

--- src/__temp_fetch.js
+++ src/__temp_fetch.js
@@ -1,26 +1,28 @@
 
-const api = "https://www.totv.com"
-
-// move this to function generating a user session
+const api = "http://localhost:3001/";
 
 const headers = {
   'Accept': 'application/json',
   'Authorization': Math.random().toString(36).substr(-8)
 }
 
+/*
+  get('categories')
+*/
+
 export const get = (path) =>
   fetch(`${api}${path}`, { headers })
     .then(res => res.json())
-    .then(data => data.children)
+    .then(data => data.children) ;
+
 
 export const update = (path, object) =>
-  fetch(`${api}${path}` ,
-    {
-        method: 'PUT', //'POST'
-        headers: {
-        ...headers,
-        'Content-Type': 'application/json'
-        } ,
-    body: JSON.stringify({ object })
-  }).then(res => res.json())
+  fetch(`${api}${path}` , {
+          method: 'PUT', //'POST'
+          headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+            } ,
+          body: JSON.stringify({ object }) })
+    .then(res => res.json())
     .then(data => data.children)
