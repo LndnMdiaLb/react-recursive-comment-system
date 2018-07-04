dc74aee421a63e999907c970acfafb845f168686
LndnMdiaLb <projects@thelondonmedialab.com>
2018-07-04 12:52:06 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,82 +1,107 @@
 import React, { Component } from 'react';
-
-import './App.css';
 import { Provider } from 'react-redux' ;
+import {BrowserRouter, Switch, Route, Link } from 'react-router-dom' ;
+
+
+import { requestRemotePostsAction, requestRemoteCategoriesAction } from './redux/asyncActions' ;
+import { userLoginAction } from './redux/actions' ;
+
 
 import { createStore, applyMiddleware, compose } from 'redux' ;
 import thunk from 'redux-thunk';
 import { reducer } from './redux/reducers';
-import { statetree } from './redux/statetree' ;
 
-import { generateTree } from './utilities/utilities' ;
-
-import { getRemotePosts } from './utilities/network' ;
 
 import {Thread} from './components/Threads' ;
 
+
+import './App.css';
+
+
+
+
 /*
-  Component :
+
+    Component :
+
     connects app to the network.js API
     uses network api to hydrate statetree template
     generates redux store passing middleware, reducers and default statetree
 
     updates react UI with connected <Threads>
+
 */
+
+const store= createStore(
+        reducer,
+        compose( applyMiddleware(thunk) ,
+                window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+            )) ;
+
+
+const Spinner= _=><div className='spinner'></div> ;
+
+
+class Login extends React.Component {
+
+    state = { user: "anonymous" } ;
+    constructor(props) { super(props) ; }
+
+    update= ({target:{value:user}})=> this.setState(_=>({ user })) ;
+
+    render=_=> {
+        const { user }= this.state ;
+        return (    <div className='login'>
+                        <span> log in as:</span>
+                        <input onChange={this.update}/>
+                        <Link to={ `/${user}` }>
+                            <button> Login </button>
+                        </Link>
+                    </div>  ) } ;
+    }
+
 
 class App extends Component {
 
-state={
-  renderContent:undefined
-}
+    state={ hydrated:false }
 
-componentDidMount(){
+    componentDidMount(){ this.getRemote(); }
 
-    getRemotePosts()
-      // hydrate statetree template
-      .then(({data})=> {
-          statetree.posts = data ;
-          return statetree })
-      // hydrate statetree template
-    // getRemoteCategories()
-      // compose redux store
-      .then(statetree=> {
-          const store = createStore(
-              reducer ,
-              statetree ,
-              compose( applyMiddleware(thunk),
-                      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()    )
-          ) ;
-          return { statetree , store } })
+    getRemote=_=>
+        Promise.all([
+            store.dispatch(requestRemotePostsAction()),
+            store.dispatch(requestRemoteCategoriesAction()) ])
+        .then(_=>this.allowThreads()) ;
 
-      // use utilites.js  to build tree
-      .then(
-        ({statetree:{posts}, store})=>
-          ({store, tree:generateTree(posts)})
-      )
-      // compose redux store
-      .then(
-        // curry params because React Reasons
-        ({ store, tree })=>
-          this.setState(prev=>({
-            renderContent:_=>
-              <Provider {...{store}}>
-                <Thread {...{tree}}/>
-              </Provider>
-              }) ))  }
+    allowThreads=_=>
+        this.setState(prev=>
+            ({  hydrated:true,
+                posts:  store.getState().posts,
+                user:   store.getState().user }) ) ;
 
+    render=_=>
+        <Provider {...{ store }}>
+            <BrowserRouter>
+                <main>
+                    <Switch>
 
-  render() {
-    const { renderContent } = this.state ;
-    return (
-      <React.Fragment>
-        {
-          renderContent
-            ? renderContent()
-            : <span>waiting</span>
-        }
-      </React.Fragment>
-    )
-  }
-}
+                        <Route
+                            exact path='/'
+                            component= { Login } />
+
+                        <Route
+                            path='/:user'
+                            render= {
+                                ({ match:{ params:{ user }}})=>{
+                                    const {user:current, hydrated, posts} = this.state ;
+                                    current !==user && store.dispatch( userLoginAction(user) ) ;
+                                    return hydrated
+                                        ? <Thread posts={ posts }/>
+                                        : <Spinner  /> }  } />
+                    </Switch>
+                </main>
+            </BrowserRouter>
+        </Provider> ;
+} ;
 
 export default App;
